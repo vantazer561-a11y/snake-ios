fe8e4fc272b1f2364315f2ffa6269e82256f9eb3
Temirbolat Temirlan <vantazer561@gmail.com>
2026-05-28 15:43:51 +0500

    F1: neutralize legacy snake model file

--- SnakeGame/Models/GameModels.swift
+++ SnakeGame/Models/GameModels.swift
@@ -1,41 +1,4 @@
 import Foundation
 
-struct Point: Hashable {
-var x: Int
-var y: Int
-}
-
-enum Direction {
-case up, down, left, right
-
-var delta: Point {
-    switch self {
-    case .up:    return Point(x: 0, y: -1)
-    case .down:  return Point(x: 0, y: 1)
-    case .left:  return Point(x: -1, y: 0)
-    case .right: return Point(x: 1, y: 0)
-    }
-}
-
-var opposite: Direction {
-    switch self {
-    case .up: return .down
-    case .down: return .up
-    case .left: return .right
-    case .right: return .left
-    }
-}
-}
-
-enum GameState {
-case idle
-case playing
-case paused
-case gameOver
-}
-
-struct GameConfig {
-static let columns = 20
-static let rows = 28
-static let tickInterval: TimeInterval = 0.18
-}
+// Legacy file from the prior Snake project. Kept empty to preserve folder structure.
+// All gameplay logic now lives in RaceGame.swift.
